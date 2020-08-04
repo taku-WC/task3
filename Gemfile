e52e7352a3668f7c2e0487418850a7fc5d489a76
taku-WC <pio.neer.bi10@gmail.com>
2020-08-04 17:56:24 +0900

    start&finish

--- Gemfile
+++ Gemfile
@@ -67,3 +67,5 @@ gem "refile-mini_magick"
 
 gem 'bootstrap-sass', '~> 3.3.6'
 gem 'jquery-rails'
+
+gem 'font-awesome-sass', '~> 5.4.1'
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update]
+  resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update] do
+  resource :favorites, only: [:create, :destroy]
+  resources :book_comments, only: [:create, :destroy]
+end
+
   resources :users, only: [:show, :index, :edit, :update, :destroy]
-
-
   root 'books#top'
 
   get '/home/about' => 'books#about'
